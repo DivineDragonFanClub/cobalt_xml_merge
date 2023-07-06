01da82ee71c6b7372577ae5d455606b85b3c1f0b
Chrom!Sevy <82708310+chrom-sevy@users.noreply.github.com>
2023-07-06 11:51:54 +0200

    Create rust.yml
    a
    
    unix path
    
    correct case sensitivty

--- src/tests.rs
+++ src/tests.rs
@@ -1,34 +1,34 @@
 use super::*;
 
-const ASSET_TABLE: &str = r"test_files\AssetTable\!original.xml";
+const ASSET_TABLE: &str = "test_files/AssetTable/!original.xml";
 
 #[test]
 fn boamo() {
     let patches = [
-        r"test_files\AssetTable\Boamo.xml"
+        "test_files/AssetTable/Boamo.xml"
     ];
     let merged = two_d_array_merge(ASSET_TABLE, &patches);
-    let boamo = read_into_lines(r"test_files\AssetTable\Boamo.xml");
+    let boamo = read_into_lines("test_files/AssetTable/Boamo.xml");
     assert!(merged == boamo);
 }
 
 #[test]
 fn playableboss() {
     let patches = [
-        r"test_files\AssetTable\PlayableBoss.xml"
+        "test_files/AssetTable/PlayableBoss.xml"
     ];
     let merged = two_d_array_merge(ASSET_TABLE, &patches);
-    let playableboss = read_into_lines(r"test_files\AssetTable\PlayableBoss.xml");
+    let playableboss = read_into_lines(r"test_files/AssetTable/PlayableBoss.xml");
     assert!(merged == playableboss);
 }
 
 #[test]
 fn boamo_playableboss() {
     let patches = [
-        r"test_files\AssetTable\Boamo.xml",
-        r"test_files\AssetTable\PlayableBoss.xml"
+        "test_files/AssetTable/Boamo.xml",
+        "test_files/AssetTable/PlayableBoss.xml"
     ];
     let merged = two_d_array_merge(ASSET_TABLE, &patches);
-    let boamo_playableboss = read_into_lines(r"test_files\AssetTable\examples\Boamo_PlayableBoss.xml");
+    let boamo_playableboss = read_into_lines("test_files/AssetTable/examples/Boamo_Playableboss.xml");
     assert!(merged == boamo_playableboss);
 }
