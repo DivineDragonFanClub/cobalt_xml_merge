b7f5ea01fc5dd6d1f62dc16890138982753d4e14
chrom-sevy <82708310+chrom-sevy@users.noreply.github.com>
2023-10-07 22:45:55 +0200

    et rid of field merging

--- src/merger.rs
+++ src/merger.rs
@@ -43,28 +43,31 @@ impl<'xml> Merger<'xml> {
                     self.merge_arena[span.a_start..span.a_end].iter_mut().for_each(|line| line.deleted = true);
                 },
                 Tag::Replace => {
-                    for (a, b) in (span.a_start..span.a_end).zip(span.b_start..span.b_end) {
-                        let line = &mut self.merge_arena[a];
-                        let patch_line = &patch[b];
-                        line.patch_params(patch_line);
-                    }
+                    self.merge_arena[span.a_start..span.a_end].iter_mut().for_each(|line| line.deleted = true);
+                    let line = &mut self.merge_arena[span.a_start];
+                    line.insert_below(&patch[span.b_start..span.b_end]);
+                    // for (a, b) in (span.a_start..span.a_end).zip(span.b_start..span.b_end) {
+                    //     let line = &mut self.merge_arena[a];
+                    //     let patch_line = &patch[b];
+                    //     line.patch_params(patch_line);
+                    // }
 
-                    // clean up trailing lines from either span
-                    let a_len = span.a_end - span.a_start;
-                    let b_len = span.b_end - span.b_start;
+                    // // clean up trailing lines from either span
+                    // let a_len = span.a_end - span.a_start;
+                    // let b_len = span.b_end - span.b_start;
 
-                    match Ord::cmp(&a_len, &b_len) {
-                        // append to the end of a
-                        Ordering::Less => {
-                            let line = &mut self.merge_arena[span.a_end-1];
-                            line.insert_below(&patch[span.b_start+a_len..span.b_end]);
-                        },
-                        // delete trailing a lines
-                        Ordering::Greater => {
-                            self.merge_arena[span.a_start+b_len..span.a_end].iter_mut().for_each(|line| line.deleted = true);
-                        },
-                        Ordering::Equal => {}
-                    }
+                    // match Ord::cmp(&a_len, &b_len) {
+                    //     // append to the end of a
+                    //     Ordering::Less => {
+                    //         let line = &mut self.merge_arena[span.a_end-1];
+                    //         line.insert_below(&patch[span.b_start+a_len..span.b_end]);
+                    //     },
+                    //     // delete trailing a lines
+                    //     Ordering::Greater => {
+                    //         self.merge_arena[span.a_start+b_len..span.a_end].iter_mut().for_each(|line| line.deleted = true);
+                    //     },
+                    //     Ordering::Equal => {}
+                    // }
 
                 },
                 _ => {}
